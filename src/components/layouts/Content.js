b41778b4ef701c6738d8d23b95c19102795146bc
dearyeon <d_earyeon@naver.com>
2021-11-06 05:05:27 +0900

    form box click add router

--- src/components/layouts/Content.js
+++ src/components/layouts/Content.js
@@ -1,13 +1,16 @@
 import '../../App.css';
 import { Container } from "@material-ui/core";
 import FormBox from '../FormBox';
+import { Link } from 'react-router-dom';
 
 function Content() {
 
     return (
         <Container>
             <h4 style={{color:"rgb(120, 120, 120)"}}>Workspace</h4>
-            <FormBox />
+            <Link to="/test">
+                <FormBox />
+            </Link>
         </Container>
     );
 }
