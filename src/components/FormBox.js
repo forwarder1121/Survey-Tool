24f043de71f53acc5f8fdce57fa9cf0ae3b1df8c
dearyeon <d_earyeon@naver.com>
2021-11-06 04:25:04 +0900

    form box css modify

--- src/components/FormBox.js
+++ src/components/FormBox.js
@@ -1,9 +1,11 @@
 import '../App.css';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
 
 function FormBox() {
     return (
         <div className="form-box">
-            New <br/> From
+            <div className="form-box-title">New From</div>
+            <AddRoundedIcon fontSize="large" style={{ color:"rgb(80, 80, 80)", margin: "35px 40px" }} />
         </div>
     );
 }
