55d2ba7eb9493f1b8f9014a5dc40967d11a5c6e7
dearyeon <d_earyeon@naver.com>
2021-11-06 05:17:21 +0900

    modify css

--- src/components/FormBox.js
+++ src/components/FormBox.js
@@ -5,7 +5,7 @@ function FormBox() {
     return (
         <div className="form-box">
             <div className="form-box-title">New From</div>
-            <AddRoundedIcon fontSize="large" style={{ color:"rgb(80, 80, 80)", margin: "35px 40px" }} />
+            <AddRoundedIcon fontSize="large" style={{ color:"rgb(80, 80, 80)", padding: "43px" }} />
         </div>
     );
 }
