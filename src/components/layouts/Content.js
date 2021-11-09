2351fd757ea2672f500f11042668e10cfe7aab3f
dearyeon <d_earyeon@naver.com>
2021-11-09 20:51:26 +0900

    content-subtitle

--- src/components/layouts/Content.js
+++ src/components/layouts/Content.js
@@ -1,17 +1,31 @@
 import '../../App.css';
-import { Container } from "@material-ui/core";
+import { Container, Button } from "@material-ui/core";
 import FormBox from '../FormBox';
 import { Link } from 'react-router-dom';
+import { MoreHoriz } from "@material-ui/icons";
 
 function Content() {
 
     return (
-        <Container>
-            <h4 style={{color:"rgb(120, 120, 120)"}}>Workspace</h4>
-            <Link to="/formPage">
-                <FormBox />
-            </Link>
-        </Container>
+        <div className="content">
+            <Container>
+                <div>
+                    <div className="content-header">
+                        <div style={{ float: "left" }}>기본 프로젝트</div>
+                        <div style={{ marginLeft: "150px" }}>
+                            <MoreHoriz color="action" fontSize="small" />
+                        </div>
+                        <div>
+                            <Button variant="contained" style={{ backgroundColor: "black", color: "white" }}>템플릿 갤러리</Button>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <Link to="/formPage">
+                    <FormBox />
+                </Link>
+            </Container>
+        </div>
     );
 }
 export default Content;
