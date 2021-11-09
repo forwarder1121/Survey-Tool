0c7016bba7503c03282eb2958e94dd75e26a4afe
dearyeon <d_earyeon@naver.com>
2021-11-09 20:14:27 +0900

    add search bar

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,12 +1,23 @@
 import '../../App.css';
+import SearchBar from "material-ui-search-bar";
 
 function Header() {
     return (
         <header>
-            <div>
-                Survey Tool
+            <div className="header-search">
+                <SearchBar 
+                    placeholder="제목 또는 내용을 검색하세요."
+                />
             </div>
         </header>
     );
 }
 export default Header;
+
+/* 
+<SearchBar
+    value={this.state.value}
+    onChange={(newValue) => this.setState({ value: newValue })}
+    onRequestSearch={() => doSomethingWith(this.state.value)}
+/>
+*/
