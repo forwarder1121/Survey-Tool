9a2bdfdaa37c36b4cef406cc682c299054ba1f52
dearyeon <d_earyeon@naver.com>
2021-11-09 18:32:29 +0900

    menu css modify

--- src/components/layouts/Menu.js
+++ src/components/layouts/Menu.js
@@ -3,8 +3,11 @@ import { Home } from "@material-ui/icons";
 function Menu() {
     return (
         <div className="menu-wrapper">
-            <div className="workspaces">Workspaces</div>
-            <hr/>
+            <div className="menu-title">이름(UI form?)</div>
+            <div className="menu-userInfo">
+                <div style={{ marginBottom: "5px" }}>사용자 이름</div>
+                <div className="user-email">email_ID@gmail.com</div>
+            </div>
             <div className="menu">
                 <ul>
                     <li><Home />My workspace</li>
