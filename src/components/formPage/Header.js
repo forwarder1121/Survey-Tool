dd691b78881cc29b88d31d7fbb2cab1d98d60e65
dearyeon <d_earyeon@naver.com>
2021-11-17 19:18:36 +0900

    header 수정

--- src/components/formPage/Header.js
+++ src/components/formPage/Header.js
@@ -1,17 +1,17 @@
-import { Link } from 'react-router-dom';
-import { Grid } from "@material-ui/core";
+import { Link } from 'react-router-dom'
+import { Grid, Button } from "@material-ui/core"
+import { AccountCircle } from "@material-ui/icons"
+
 
 function Header(props) {
     const action = props.action;
     return (
         <header>
             <Grid container spacing={1}>
-                <Grid item xs={12} sm={2}>
+                <Grid item sm={3}>
                     <div style={{ fontWeight: "600" }}>프로젝트명 / 현재설문이름</div>
                 </Grid>
-                <Grid item xs={12} sm={1}>
-                </Grid>
-                <Grid item xs={12} sm={9}>
+                <Grid item sm={7}>
                     <div className="hor-menu">
                         <Link to="/form/create">
                             <div className={ action === "create" ? "hor-menu-ele-act" : "hor-menu-ele" }>제작/설계</div>
@@ -26,6 +26,12 @@ function Header(props) {
                             <div className={ action === "result" ? "hor-menu-ele-act" : "hor-menu-ele" }>결과확인</div>
                         </Link>
                     </div>
+                </Grid>
+                <Grid item sm={2} style={{ textAlign:"right" }}>
+                    <Button variant="contained" color="primary" size="small">회사정보</Button>
+                    <span style={{ padding: "0px 10px", position: "relative", top: "3px"}}>
+                        <AccountCircle  fontSize="large" />
+                    </span>
                 </Grid>
             </Grid>
         </header>
