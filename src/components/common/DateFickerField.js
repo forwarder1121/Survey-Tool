8df85438d0221f0a7c2a56abd7ea71d587a53ab3
dearyeon <d_earyeon@naver.com>
2021-11-28 23:11:22 +0900

    test

--- src/components/common/DateFickerField.js
+++ src/components/common/DateFickerField.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 function DateFickerField() {
     const [date, setDate] = React.useState(new Date());//Date().toLocaleString()
@@ -9,7 +12,15 @@ function DateFickerField() {
     };
     return (//props -> 
         <div>
-            
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+
+                <DateTimePicker
+                    label="DateTimePicker"
+                    inputVariant="outlined"
+                    value={date}
+                    onChange={handleChange}
+                />
+            </MuiPickersUtilsProvider>
         </div>
     );
 }
